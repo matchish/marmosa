4c3b0c9863a4b20a0c8ccc11870c12bf4fd7e665
Serhii Shliakhov <husband.sergey@gmail.com>
2026-03-19 08:10:03 +0000

    Remove purely C# attribute tests that don't apply to rust's type system

--- tests_opossum/Opossum.UnitTests/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using System.Collections.Concurrent;
-global using System.Text.Json;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Logging;
-global using Xunit;
